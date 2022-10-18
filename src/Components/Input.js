fa13eb540edd8b75cdb89c759c7ae7039196fb6b
Emiliedebang <91943014+Emiliedebang@users.noreply.github.com>
2022-10-18 12:28:00 +0200

    layout

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,17 +3,12 @@ import React from 'react'
 function Input() {
   return (
     <div className = "input-container">
-     <input type = "number" className = "TestNumber"> 
-      </input>
-      <input type = "date" className = "Date">
-
-      </input>
-
-      <input type = "number" className = "TestValue">
-      </input>
-<p className = "validity">
-Valid
-</p>
+      <input type = "number" className = "TestNumber"/> 
+      <input type = "date" className = "Date"/>
+      <input type = "number" className = "TestValue"/>
+      <p className = "validity">
+        Valid
+      </p>
 
 
 
