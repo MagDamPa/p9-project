2f4aa7cdbf7e7a38cc55bad444cde9b6a53bb806
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2022-10-18 11:20:12 +0200

    Menu button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Menu from './Components/Menu';
 
 function App() {
   return (
     <div className="App">
-   
+      <div className="top-flexbox">
+        <Menu>
+
+        </Menu>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import menuIcon from './menuIcon.svg'
+
+
+
+function Menu() {
+
+
+    function buttonHandler () {
+        console.log("Touch me baby");
+    }
+  return (
+    <div>
+        <button
+            onClick={buttonHandler}
+        >
+            <img 
+                src={menuIcon}
+            />
+        </button>
+    </div>
+  )
+}
+
+export default Menu
