dc5fc65d28619726544c0774b2f5d93e4b1ec37d
MagDamPa <92024310+MagDamPa@users.noreply.github.com>
2022-10-18 12:23:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import Menu from './Components/Menu';
 import Help from './Components/Help';
-
+import Input from './Components/Input'
 function App() {
   return (
     <div className="App">
       <div className="top-flexbox">
         <Menu/>
         <Help/>
+      </div>
+      <div className ="Input">
+        <Input/>
+ 
       </div>
     </div>
   );
