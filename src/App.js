113b2744f87d3cfdc93b4e3fe2f5565ece16d4ad
Emiliedebang <91943014+Emiliedebang@users.noreply.github.com>
2022-11-11 21:03:17 +0100

    It works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Menu from './Components/Menu';
 import Help from './Components/Help';
@@ -8,14 +8,11 @@ import Chart2 from './Components/Chart2';
 import {answers, convertNgMg} from './Utils/Model'; 
 import Result from './Components/Result';
 import AddIcon from './Components/Icons/AddIcon.svg'
-import { v4 as uuidv4 } from 'uuid'
 import { specimen_base, specimen_last } from './Utils/Model';
 
 function App() {
 
   const [datapoints, setDatapoints] = useState([]);
-  const testDateRef = useRef()
-  const testValueRef = useRef()
 
   const [, updateState] = React.useState();
   const forceUpdate = React.useCallback(() => updateState({}), []);
@@ -27,21 +24,7 @@ function App() {
     } 
     else {
     }
-  }, [datapoints.length]);
- 
-  function buttonHandlerAdd(e){
-    e.preventDefault();
-    const date = testDateRef.current.value
-    const value = testValueRef.current.value
-    setDatapoints(prevDatapoints => {
-      return [...prevDatapoints, { Id: uuidv4(), Number: datapoints.length, Date: date, Value: value, Valid: 'Valid'}]
-    })
-    testDateRef.current.value = null
-    testValueRef.current.value = null
-  }
-
-
-  
+  }, [datapoints.length]);  
 
   return (
     <div className="App">
@@ -54,22 +37,15 @@ function App() {
           <Input 
             datapoints={datapoints} 
             setDatapoints={setDatapoints}
-            testDateRef={testDateRef}
-            testValueRef={testValueRef}
           />
-          <button type='submit' className='Add' onClick={buttonHandlerAdd}>
-          Tilføj testresult
-          </button>
         </div>
         <div className='ResultBlock'>
           <Result 
           answersTitle={answers.Title} 
           answersText={answers.Text}
           answersColor={answers.Color}
+          answersCalculation={answers.Calculation}
           />
-          <p className='UsedTest'>
-            Modellen har beregnet resultatet på baggrund af test nr. {specimen_base + 1} og test nr. {specimen_last + 1}.
-          </p>
         </div>
       </div>
       <div className = "result-and-chart-wrapper">
