511469a52b33abf7f5966bcda4cdb37124d55d3e
MagDamPa <92024310+MagDamPa@users.noreply.github.com>
2022-11-23 11:35:43 +0100

    Create Render.js

---
+++ src/Components/Render.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Render(){
+
+      
+ function print()
+ {
+    window.print()
+ }
+
+
+return (
+<button className='Render' onClick={print}>
+Udskriv eller gem som PDF
+</button>
+)
+}
+export default Render
