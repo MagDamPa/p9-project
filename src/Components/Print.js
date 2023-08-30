206dd2c1213bceda1da9f9ff242c0be260305de9
MagDamPa <92024310+MagDamPa@users.noreply.github.com>
2023-08-30 13:44:25 +0200

    Update Print.js

--- src/Components/Print.js
+++ src/Components/Print.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../Style/Render.css';
 
 function Render(){
-
+// The Print.js function is a quite simple function, which originates from standard javascript. It essentially calls this function, asks the system print to print the entirity of the page,
+// and finally, it is returned to our app.js.
       
  function print()
  {
